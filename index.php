e156677a11b5bc561f3c2d8f445211cf7f997bc7
user2 <user2@HAL-PC>
2014-07-14 14:34:15 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,1 @@
+<h2>exoweb.pl</h2>
---
+++ test/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"> 
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <div id="navbar">
+        <div class="container">
+            <div id="logo">
+                EXOWEB
+            </div>
+            <div id="menu">
+                <a href="#" class="menu-link">Strona główna</a>
+                <a href="#" class="menu-link">Oferta</a>
+                <a href="#" class="menu-link">Kontakt</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="hero-unit">
+            <h1>Witaj na stronie Exoweb</h1>
+            <p>Strona www z darmową opieką techniczną przez 6 miesięcy. 
+                <a href="#">Zobacz szczegóły.</a></p>
+            <p>Dzięki technologiom responsywnym (ang. <i>responsive desing</i>) 
+                Twoja strona będzie poprawnie wyświetlać się na różnych urządzeniach.</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="margin-left-20">
+            <div class="block1">
+                <h2>block-a</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. </p>
+            </div>
+            <div class="block1">
+                <h2>block-b</h2>
+                <p>Donec facilisis vel augue at varius. Vivamus venenatis, 
+                    nisl non sagittis tempus, tellus sem laoreet magna, 
+                    at sodales nunc nisl in urna.</p>
+            </div>
+            <div class="block1">
+                <h2>block-c</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <hr>
+        <p>© Exoweb 2014</p>
+    </div>
+
+    
+    
+    
+            
+    
+        
+
+</body>
+</html>
+
+
+<?php
+
