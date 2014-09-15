fdc74bed82ce6913cbda51604ebcc052cda91181
pio <pio@arch>
2014-09-15 11:24:07 +0200

    added some text

--- test/index.php
+++ test/index.php
@@ -6,13 +6,19 @@ include 'header.php';
     <div class="container">
         <div class="hero-unit">
             <h1>Witaj!</h1>
+            <p>Exoweb oferuje projektowanie witryn oraz średnich aplikacji 
+                internetowych.</p>
         </div>
     </div>
     <div class="container">
         <div class="margin-left-20">
             <div class="block1">
                 <h2>Sprawdzone technologie</h2>
-                <p>Projektowanie witryn na najnowsze urządzenia. 
+                <p>
+                    Projektowanie witryn i aplikacji internetowych wymaga dziś 
+                    zastosowania odpowiednich rozwiązań. Exoweb wykorzystuje najnowsze 
+                    technologie oraz testuje swoje produkty na różnych systemach 
+                    i wyświetlaczach.
                 </p>
                 <p>
                     <img src="img/Responsive-Design-Multi-Device-Example-lq.jpg" />
@@ -21,18 +27,18 @@ include 'header.php';
             <div class="block1">
                 <h2>Optymalne rozwiązania</h2>
                 <p> 
-                    <!--img src="img/loadspeed-small.jpg" style="padding:5px; float:right"/ -->
+                    <!--img src="img/loadspeed-small.jpg" style="padding:5px; float:right" -->
                     Szybkość wczytywania witryny jest jednym z decydujących 
                     czynników wpływających na pozycję strony w wyszukiwarkach  
-                    oraz ogólne wrażenia odwiedzającego.
+                    oraz ogólne wrażenia odwiedzającego. Co więcej to właśnie pierwsze 
+                    sekundy bardzo często decydują czy internauta to pozostania na stronie.
                 </p>
             </div>
             <div class="block1">
                 <h2>Pełna pomoc techniczna</h2>
                 <p>W cenę wykonania strony wliczona jest pomoc w wyborze 
                  domeny, serwera oraz instalacja strony na serwerze.</p>
-                <p>Każda strona otrzymuje 6 miesięcy opieki technicznej.
-                <a href="#">zamów stronę</a></p>
+                <p>Każdy produkt otrzymuje gwarancję. <a href="#">zobacz więcej</a></p>
             </div>
         </div>
     </div>
